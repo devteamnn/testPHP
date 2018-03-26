680e4d3db33c2ba1836323fc23ceea7793d4b14c
Dmitriy Kononov <kondor_06@mail.ru>
2018-03-26 23:39:30 +0300

    333

--- remains_make_pdf.php
+++ remains_make_pdf.php
@@ -1,106 +1,54 @@
 <?php
+require_once 'external_scripts/tfpdf/tfpdf.php';
 
-function remainsMakePdf() {
-	require('external_scripts/fpdf.php');
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
-class PDF extends FPDF
-{
-// Load data
-function LoadData($file)
-{
-    // Read file lines
-    $lines = file($file);
-    $data = array();
-    foreach($lines as $line)
-        $data[] = explode(';',trim($line));
-    return $data;
-}
+function remainsMakePdf($data, $directory, $par) {  
+  //  -------- Функции, формирующие документ --------
+  function drawHeader(&$pdf, $par) {
+    $pdf->Write(5, 'Текущая' . $pdf->Ln() . 'цена продажи:');
 
-// Simple table
-function BasicTable($header, $data)
-{
-    // Header
-    foreach($header as $col)
-        $this->Cell(40,7,$col,1);
-    $this->Ln();
-    // Data
-    foreach($data as $row)
-    {
-        foreach($row as $col)
-            $this->Cell(40,6,$col,1);
-        $this->Ln();
-    }
-}
+    // $pdf->Cell(10,5, 1,'C');
+    
+    // $pdf->MultiCell(20,5,'Группа:', 1);
+    // $pdf->MultiCell(20,5,'Название:', 1);
+    // $pdf->MultiCell(20,5,'Количество:', 1);
 
-// Better table
-function ImprovedTable($header, $data)
-{
-    // Column widths
-    $w = array(40, 35, 40, 45);
-    // Header
-    for($i=0;$i<count($header);$i++)
-        $this->Cell($w[$i],7,$header[$i],1,0,'C');
-    $this->Ln();
-    // Data
-    foreach($data as $row)
-    {
-        $this->Cell($w[0],6,$row[0],'LR');
-        $this->Cell($w[1],6,$row[1],'LR');
-        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-        $this->Ln();
-    }
-    // Closing line
-    $this->Cell(array_sum($w),0,'','T');
-}
+    // if (validate_parametr($par, 'p04')) {
+    //   $pdf->Cell(100, 5, $pdf->Write(5, 'Текущая \n цена закупки'), 1, 0, 'C');
+    // }
+    
+    // if (validate_parametr($par, 'p05')) {
+    //   $pdf->Cell(100,5,'Текущая цена продажи:', 1, 0, 'C');
+    // }
+    
+    // if (validate_parametr($par, 'p02')) {
+    //   $pdf->MultiCell(100,5,'На сумму по \n закупке:', 1, 0, 'C');
+    // }
+    
+    // if (validate_parametr($par, 'p03')) {
+    //   $pdf->MultiCell(100,5,'На сумму по продаже:', 1, 0, 'C');
+    // }
+    
+    // if (validate_parametr($par, 'p01')) {
+    //   $pdf->MultiCell(100,5,'ожидает поступления на склад:', 1, 0, 'C');
+    //   $pdf->MultiCell(100,5,'ожидает отправки покупателю:', 1, 0, 'C');
+    // }
+  }
 
-// Colored table
-function FancyTable($header, $data)
-{
-    // Colors, line width and bold font
-    $this->SetFillColor(255,0,0);
-    $this->SetTextColor(255);
-    $this->SetDrawColor(128,0,0);
-    $this->SetLineWidth(.3);
-    $this->SetFont('','B');
-    // Header
-    $w = array(40, 35, 40, 45);
-    for($i=0;$i<count($header);$i++)
-        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
-    $this->Ln();
-    // Color and font restoration
-    $this->SetFillColor(224,235,255);
-    $this->SetTextColor(0);
-    $this->SetFont('');
-    // Data
-    $fill = false;
-    foreach($data as $row)
-    {
-        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
-        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
-        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
-        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
-        $this->Ln();
-        $fill = !$fill;
-    }
-    // Closing line
-    $this->Cell(array_sum($w),0,'','T');
-}
-}
+  $pdf = new tFPDF();
+  $pdf->AddPage('L');
+  $pdf->AddFont('DejaVu','','DejaVuSerifCondensed.ttf',true);
+  $pdf->AddFont('DejaVuBold','','DejaVuSerifCondensed-Bold.ttf',true);
 
-$pdf = new PDF();
-// Column headings
-$header = array('Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)');
-// Data loading
-$data = $pdf->LoadData('countries.txt');
-$pdf->SetFont('Arial','',14);
-$pdf->AddPage();
-$pdf->BasicTable($header,$data);
-$pdf->AddPage();
-$pdf->ImprovedTable($header,$data);
-$pdf->AddPage();
-$pdf->FancyTable($header,$data);
-$pdf->Output();
+  $pdf->SetFont('DejaVuBold','',6);
 
-	return 'OK';
+  drawHeader($pdf, $par);
+
+  // $pdf->Output('test.pdf');
+  $pdf->Output();
+
+  return 'pk'; 
 }
