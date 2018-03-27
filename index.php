9ca5a7a9ca382498be20fa779b27a48f8b4df0ef
Dmitriy Kononov <kondor_06@mail.ru>
2018-03-27 16:24:01 +0300

    ggg

--- index.php
+++ index.php
@@ -1,84 +1,69 @@
 <?php
-require_once 'external_scripts/fpdf/fpdf.php';
+// require_once 'external_scripts/fpdf/fpdf.php';
 
   ini_set('error_reporting', E_ALL);
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
 
-  // function validate_parametr($str, $parmetr) {
-  //   if (strpos($str, $parmetr) === false) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
+  function validate_parametr($str, $parmetr) {
+    if (strpos($str, $parmetr) === false) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   
-  // // require_once 'remains_make_xlsx.php';
+  require_once 'remains_make_xlsx.php';
   // require_once 'remains_make_pdf.php';
   
-  // $data = [
+  $data = [
  
-  //     "business_name"=>"klop22322",
-  //     "current_time"=>1521443363,
-  //     "content"=>[
-  //       [
-  //         "group_name"=>"GGG222",
-  //         "group_content"=>[
+      "business_name"=>"klop22322",
+      "current_time"=>1521443363,
+      "content"=>[
+        [
+          "group_name"=>"GGG222",
+          "group_content"=>[
 
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"]
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"]
 
-  //         ]
-  //       ],
-  //       [
-  //         "group_name"=>"GGG222",
-  //         "group_content"=>[
+          ]
+        ],
+        [
+          "group_name"=>"GGG222",
+          "group_content"=>[
 
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"],
-  //           ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
-  //               "price_purchase"=>"30.000","price_sell"=>"36.000"]
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"],
+            ["good_id"=>"53", "good_name"=>"корзиночка","good_count"=>"7.000",
+                "price_purchase"=>"30.000","price_sell"=>"36.000"]
 
-  //         ]
-  //       ]     
-  //     ]
-  // ];
+          ]
+        ]     
+      ]
+  ];
   
-  // $par = '[p02,p03,p04,p05,p06]';
+  $par = '[p02,p03,p04,p05,p06]';
   
-  // // echo remainsMakeXlsx($data, '11', $par);
+  echo remainsMakeXlsx($data, '11', $par);
   // echo remainsMakePdf($data, '111', $par);
-
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-
-$pdf->MultiCell(50,6,'111111111111111111111' . PHP_EOL . '22222222222222222', 1);
-$x = $pdf->getX();
-$y = $pdf->getY();
-$pdf->SetXY($x + 50, 10);
-$pdf->MultiCell(50,6,'111111111111111111111' . PHP_EOL . '22222222222222222111111111111111111111111111111', 1);
-
-
-
-$pdf->Output();
- 
  
